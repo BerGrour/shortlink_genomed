ed60cc30099d2f2ad419efc42ee6edc1789dbe08
Rowl <BerGrour@mail.ru>
2024-09-22 10:28:54 +0300

    added uniqueness property for short_link attribute

--- migrations/m240921_123056_create_links_table.php
+++ migrations/m240921_123056_create_links_table.php
@@ -15,7 +15,7 @@ class m240921_123056_create_links_table extends Migration
         $this->createTable('{{%links}}', [
             'id' => $this->primaryKey(),
             'original_url' => $this->text()->notNull(),
-            'short_url' => $this->string()->notNull(),
+            'short_url' => $this->string()->notNull()->unique(),
             'clicks' => $this->integer()->defaultValue(0)->notNull(),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull()
